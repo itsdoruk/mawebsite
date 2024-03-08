a2b2b5ff9358f5a59701c88ec53b7b4b6dba0671
itsdoruk <155773201+itsdoruk@users.noreply.github.com>
2024-03-08 21:32:33 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
 };
-
 export default nextConfig;
+
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
