eacff7be193c6ab189bf5b9b43b3aa5a2bb7e8cf
itsdoruk <155773201+itsdoruk@users.noreply.github.com>
2024-03-08 21:55:28 +0300

    Update meta.tsx

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,29 +1,32 @@
-import Head from "next/head";
+import Head from "next/head"
+import { useRouter } from "next/router"
 
 export default function Meta() {
-   const title = "dSplash.xyz: born just in time to explore computers";
-   const description = "student by day, open source coder by night. ";
-   const keywords =
-      "istanbul, hack club, doruk sarp aydın, dsplash, react, nextjs, bash, young developer, linux";
-   const author = "Doruk Sarp Aydın";
-   const image =
-      "https://cloud-e87zjwx91-hack-club-bot.vercel.app/0coral_1fab8.png";
-   return (
-      <Head>
-         <meta charSet="utf-8" />
-         <meta name="viewport" content="width=device-width,initial-scale=1" />
-         <title>{title}</title>
-         <meta name="description" content={description} />
-         <meta name="keywords" content={keywords} />
-         <meta name="author" content={author} />
-         <meta property="og:url" content="https://aram.sh" />
-         <meta property="og:type" content="website" />
-         <meta property="og:title" content={title} />
-         <meta property="og:description" content={description} />
-         <meta property="og:image" content={image} />
-         <meta name="twitter:card" content="summary_large_image" />
-         {children}
-      </Head>
-   );
-}
+    const router = useRouter()
+    const meta = {
+        title: `${router.pathname === "/" ? "" : ` ${router.pathname} |`} dSplash.xyz: born just in time to explore programming.`,
+        description: "Linux user, programmer and a student",
+        image: "https://cloud-e87zjwx91-hack-club-bot.vercel.app/0coral_1fab8.png",
+        type: "website"
+    }
 
+    return (
+        <Head>
+            <title>{meta.title}</title>
+            <meta name="robots" content="follow, index" />
+            <meta content={meta.description} name="description" />
+            <meta property="og:url" content={`https://example.com/${router.asPath}`} />
+            <link rel="canonical" href={`https://example.com/${router.asPath}`} />
+            <meta property="og:type" content={meta.type} />
+            <meta property="og:site_name" content="Conifer" />
+            <meta property="og:description" content={meta.description} />
+            <meta property="og:title" content={meta.title} />
+            <meta property="og:image" content={meta.image} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@example" />
+            <meta name="twitter:title" content={meta.title} />
+            <meta name="twitter:description" content={meta.description} />
+            <meta name="twitter:image" content={meta.image} />
+        </Head>
+    )
+}
